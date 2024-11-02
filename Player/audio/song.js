680d5ab2b05eec05ac8442707f68584dcd1aed28
Aung <aungmyat1104@gmail.com>
2024-11-03 06:20:49 +0630

    initial commit

---
+++ Player/audio/song.js
@@ -0,0 +1,30 @@
+const songs = [
+	{
+		id: 1,
+		title: 'Sunflower Street Drumloop 85bpm',
+		name: 'Kamhunt',
+		song: 'audio/song1.mp3',
+		cover: 'cover/img1.webp'
+	},
+	{
+		id: 2,
+		title: '[Intro Music] BLACK BOX - Cymatics VS Slate digi',
+		name: 'Black Box',
+		song: 'audio/song2.mp3',
+		cover: 'cover/img2.jpeg'
+	},
+	{
+		id: 3,
+		title: 'kick hardstyle',
+		name: 'Van Wiese',
+		song: 'audio/song3.mp3',
+		cover: 'cover/img3.jpeg'
+	},
+	{
+		id: 4,
+		title: 'Nodens (Field Song)',
+		name: 'Pixabay',
+		song: 'audio/song4.mp3',
+		cover: 'cover/img4.jpeg'
+	}
+]
